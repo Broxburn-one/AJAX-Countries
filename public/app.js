70d911cb02db384c90956596eb0ad84db4f287cb
Borxburn-One <broxburn001@yahoo.co.uk>
2016-03-29 16:23:11 +0100

    added persistence

--- public/app.js
+++ public/app.js
@@ -1,36 +1,79 @@
 window.onload = function(){
   console.log('App started');
+
+// put any global vars here
+
   var url = 'https://restcountries.eu/rest/v1';
   var request = new XMLHttpRequest();  // now tell it where to go and the action to take
+  var selCountry;
+  var population;
+  var capital;
+  var countryList = [];
+
+
+
+// check if anything persistent
+ var storedCountry = JSON.parse(localStorage.getItem('country-stats')) || []; 
+
+// now use the persistent stuff if there is any
+ if (storedCountry.length > 0) {
+    selCountry = storedCountry[0].name;
+    population = storedCountry[1].population;
+    capital = storedCountry[2].capital;
+
+  // console.log('at the very start- stored: ', capital);
+      document.getElementById('name').innerHTML = selCountry;
+      document.getElementById('capital').innerHTML = capital;
+      document.getElementById('population').innerHTML = population;
+ } 
+
   request.open("GET", url);   // next tell it what to do when it completes = async nature of it
   request.onload = function() {
     if(request.status === 200){
-      console.log('got the day success');
-  //    console.log(request.responseText);
       var jsonString = request.responseText;
       var countries = JSON.parse(jsonString);
-  //    console.log('countries', countries);
-   //   console.log('first country', countries[0]);
 
-      var countryList = [];
 
       for(var i = 0; i < countries.length; i++) {
         countryList.push(countries[i].name)
       }
-     console.log(countryList);
+   // console.log(countryList);
 
 
    var sel = document.getElementById('pays');
-for(var i = 0; i < countryList.length; i++) {
-    var opt = document.createElement('option');
-    opt.innerHTML = countryList[i];
-    opt.value = countryList[i];
-    sel.appendChild(opt);
-}
+   for(var i = 0; i < countryList.length; i++) {
+      var opt = document.createElement('option');
+      opt.innerHTML = countryList[i];
+      opt.value = countryList[i];
+      sel.appendChild(opt);
+    }
 
+    sel.onchange = function() {
+      selCountry = sel.options[sel.selectedIndex].text;
+      //console.log('selected country: ', selCountry); 
 
+      for(var i = 0; i < countries.length; i++) {
+        if (countries[i].name === selCountry) {
+          population =  countries[i].population;
+          capital = countries[i].capital;
+        }
+      }  
+     //   console.log(capital, population);
+        document.getElementById('name').innerHTML = selCountry;
+        document.getElementById('capital').innerHTML = capital;
+        document.getElementById('population').innerHTML = population;
+// persist
+      storedCountry = [
+        {'name': selCountry},
+        {'population': population},
+        {'capital': capital}
+      ];
 
+   //   console.log('persistent countrystats: ', storedCountry);
+        localStorage.setItem('country-stats', JSON.stringify(storedCountry));
 
+    }
+ 
 
 
 
